d782c4e50f435121a71a4b9aea597070acb47660
Rubén Guerrero <yo@rubenguerrero.com>
2018-07-18 18:39:26 +0200

    More tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get a started, editing <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+const Loading = () => <div>Loading...</div>;
+const Home = Loadable({
+  loader: () => import('./pages/Home'),
+  loading: Loading
+});
+const About = Loadable({
+  loader: () => import('./pages/About'),
+  loading: Loading
+});
+const Topics = Loadable({
+  loader: () => import('./pages/Topics'),
+  loading: Loading
+});
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
 );
-
-export default App;
+export default BasicExample;
